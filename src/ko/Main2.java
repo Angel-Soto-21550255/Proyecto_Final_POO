15fa41af81ca66e6008043326a1e4cfbd935a1e1
Luis Angel Soto Hernández <94801604+Angel-Soto-21550255@users.noreply.github.com>
2022-01-05 22:08:22 -0700

    Add files via upload

---
+++ src/ko/Main2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ko;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import jfrem.FrmInfo;
+
+/**
+ *
+ * @author angel
+ */
+public class Main2 {
+    
+    public static void main(String[] args) throws IOException {
+      FrmInfo m2 = new FrmInfo();
+      m2.setVisible(true);  
+        
+        
+        
+        
+    }
+    
+}
